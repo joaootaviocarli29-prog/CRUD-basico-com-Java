4c519d67ce42d50e29f6fca4157c6da06105ffaf
joaootaviocarli29-prog <joaootaviocarli29@gmail.com>
2026-08-18 15:12:17 -0300

    fix: Correção para a entrada no banco de dados.

--- src/main/java/connection/Conexao.java
+++ src/main/java/connection/Conexao.java
@@ -9,7 +9,7 @@ public class Conexao {
     //Configurações de acesso ao MySQL
     private static final String URL = "jdbc:mysql://localhost:3306/loja";
     private static final String USUARIO = "root";
-    private static final String SENHA = "Carro2464@";
+    private static final String SENHA = ""; //Troque pela sua senha
 
     //Método para obter conexão
     public static Connection conectar() {
